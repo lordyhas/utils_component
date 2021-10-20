5c3522b19ebd2a09222525b1d564af0193871daa
lordyhas <dev.lordyhas@gmail.com>
2021-10-20 19:50:12 +0200

    commit on 2021.20.10-21.50

--- lib/src/widget/boolean_builder.dart
+++ lib/src/widget/boolean_builder.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 abstract class CaseBuilderWidget extends StatelessWidget{
   const CaseBuilderWidget({Key? key,}) : super(key: key);
 }
@@ -16,7 +15,7 @@ class ContainerBooleanBuilder extends BooleanBuilderWidget {
   final Widget
   ? ifFalse;
 
-  const ContainerBooleanBuilder({
+  ContainerBooleanBuilder({
     Key? key,
     this.check = true,
     this.ifTrue,
@@ -35,9 +34,9 @@ class BooleanBuilder extends BooleanBuilderWidget {
   final Widget ifTrue;
   final Widget ifFalse;
 
-  const BooleanBuilder({
+  BooleanBuilder({
     Key? key,
-    this.check = true,
+    required this.check,
     required this.ifTrue,
     required this.ifFalse
   }) : super(key: key);
@@ -50,22 +49,15 @@ class BooleanBuilder extends BooleanBuilderWidget {
 }
 
 class CaseBuilder extends CaseBuilderWidget{
-  final Enum status;
+
   const CaseBuilder({
     Key? key,
-    required  this.status,
-  }) : assert(status is Enum, "status must be an Enum"), super(key: key);
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Builder(
-      builder: (context) {
-        switch(status){
-          //case status.values[0]:
-        }
-        return Container();
-      }
-    );
+    return Container();
   }
 
 }
+
